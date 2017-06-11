b20abd6a340ff9513525a5258f48a5b5841458bb
RapidTheNerd <george.marr.cobc@gmail.com>
2017-06-11 23:33:29 +0100

    update method is similar to server info

--- src/main/java/me/rtn/jump/status/SignStatus.java
+++ src/main/java/me/rtn/jump/status/SignStatus.java
@@ -3,6 +3,12 @@ package me.rtn.jump.status;
 import org.bukkit.Location;
 import org.bukkit.block.Sign;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 /*
  * Jump
  * Copyright (C) 2017 RapidTheNerd
@@ -37,7 +43,39 @@ public class SignStatus {
     }
 
     public void update(){
-        
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(serverIP, port), 1 * 1000);
+
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+
+            output.write(0xFE);
+
+            StringBuilder builder = new StringBuilder();
+            int b;
+            while ((b = input.read()) != -1) {
+                if(b != 0 && b > 16 && b != 255 && b != 23 && b != 24){
+                    builder.append((char) b);
+                }
+            }
+            String[] data = builder.toString().split("&c");
+            String motd = data[0];
+            int onlinePlayers = Integer.valueOf(data[1]);
+            int maxPlayers = Integer.valueOf(data[2]);
+            /*
+                -------------
+                |Server Name|
+                |Server IP  |
+                |Server MOTD|
+                -------------
+                      |
+                      |
+             */
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Location getLocation() {
